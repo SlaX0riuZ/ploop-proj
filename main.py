8f9a3d019923a5684410da41dad914ef7e51b6f5
SlaX0riuZ <slaxoriuz@gmail.com>
2024-11-13 11:11:53 -0600

    first issue done

---
+++ main.py
@@ -0,0 +1,30 @@
+# Python-Loop Project
+# Created by: SlaX0riuZ
+# 11/13/2024
+# v0.0.01 Alpha
+
+
+'''
+For Extra "Creativity Points":
+- Allow float values for factorials!
+- Possibly implement A x 10^B notation for anything over 10^10
+'''
+
+
+print("Python-Loop Factorial Calculator")
+print("Created by SlaX0riuZ")
+print("<------------------------->")
+
+
+unum = int(input("Input the number you want to take a factorial of: "))
+
+
+def get_fct(n):
+    z = 1 # temporary variable
+    for i in range(n, 1, -1):
+        z = z * i
+    return z
+
+
+output = get_fct(unum)
+print(output)
