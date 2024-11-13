2938ecf8e22c9b87bb196171aa7e6356ffea91e7
SlaX0riuZ <slaxoriuz@gmail.com>
2024-11-13 11:44:41 -0600

    finished everything else.

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 # Created by: SlaX0riuZ
 # 11/13/2024
 # v0.0.01 Alpha
-#updated 10:46 AM
 
 '''
 For Extra "Creativity Points":
-- Allow float values for factorials!
 - Possibly implement A x 10^B notation for anything over 10^10
+- Use try/except/else/finally
 '''
 
 
@@ -15,16 +14,33 @@ print("Python-Loop Factorial Calculator")
 print("Created by SlaX0riuZ")
 print("<------------------------->")
 
+try: # Give user something to input
+    unum = int(input("Input the number you want to take a factorial of: "))
 
-unum = int(input("Input the number you want to take a factorial of: "))
+except ValueError: # User didn't input an integer.
+    print("Please input a number that's an integer.")
 
+else: # User inputted an integer.
 
-def get_fct(n):
-    z = 1 # temporary variable
-    for i in range(n, 1, -1):
-        z = z * i
-    return z
+    def get_fct(n): # Factorial Calculator's basic function
+        z = 1
+        for i in range(n, 1, -1):
+            z = z * i
+        return z
 
+    output = "" # Giving the computer something to edit.
 
-output = get_fct(unum)
-print(output)
+    if unum < 0: # Prevent Negative Factorial Error...
+        print("Error! Negative factorial numbers don't exist.")
+        print("Please input a number that's greater than or equal to 0.")
+        print("<------------------------->")
+    else:
+        output = str(get_fct(unum))
+        
+        if len(output) >= 10: # Use Scientific Notation
+            v_exp = int(len(output)) - 1
+            v_front = str(output[:1]) + "." + str(output[1:4])
+            print(str(unum) + "! is: " + str(v_front) + " x 10^" + str(v_exp))
+
+        else: # No need to use Scientific Notation
+            print(str(unum) + "! is: " + str(output))
